8d2af2a2e7ff38a8f19db18657d8d99a5983af95
Manasa <manasaaludi17@gmail.com>
2026-09-20 17:34:57 +0530

    Add Python elif practice

---
+++ Python/elif.py
@@ -0,0 +1,18 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+elif marks >= 50:
+    print("Grade D")
+else:
+    print("Fail")
+
+
+if marks >= 50:
+    print("Pass")
+elif marks >= 90:
+    print("A")
