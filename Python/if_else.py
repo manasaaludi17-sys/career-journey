e5f85a30c42ddfe30a978e9a2fe25212683cb854
Manasa <manasaaludi17@gmail.com>
2026-09-20 17:33:42 +0530

    Add Python if else practice

---
+++ Python/if_else.py
@@ -0,0 +1,22 @@
+age = 20
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+marks = 75
+
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+
+mark = int(input("Enter your marks: "))
+
+if mark >= 50:
+    print("Pass")
+else:
+    print("Fail")
