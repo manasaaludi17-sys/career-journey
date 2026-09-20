7a6187282c0d9766f2676aecfcf3409d309200b6
Manasa <manasaaludi17@gmail.com>
2026-09-20 17:30:30 +0530

    Add Python variables practice

--- Python/variables.py
+++ Python/variables.py
@@ -5,3 +5,38 @@ height = 5.5
 print(name)
 print(age)
 print(height)
+
+name = "Manasa"
+age = 20
+height = 5.5
+city = "Tirupati"
+
+print(name)
+print(age)
+print(height)
+print(city)
+
+name = "Manasa"
+age = 20
+height = 5.5
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+college = "ABC College"
+marks = 85
+print(type(college))
+print(type(marks))
+
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+age = input("Enter your age: ")
+print(type(age))
+
+age = int(input("Enter your age: "))
+
+print(age)
+print(type(age))
